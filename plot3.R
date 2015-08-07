044e9b6920e5b9f71731aeaf1b2c7af88542cf1d
Kevin E. D'Elia <KevinDElia@verizon.net>
2015-08-07 17:31:57 -0400

    final versions of R code and PNG files for plot3

--- plot3.R
+++ plot3.R
@@ -1,33 +1,59 @@
-# Section One ----
+#
+# R code for Course Project 1 in the 
+# Coursera course "Exploratory Data Analysis"
+#
+# This file creates plot3.png according to the plot given in the assignment
+
+# Section One: Get the data ----
+#
+# set the source of the data 
 dataFile <- "./data/household_power_consumption.txt"
 
-conn <- file(dataFile, open = "rt")
+# create a read-only connection to the text file
+connection <- file(dataFile, open = "rt")
 
+# loop control variable
 x <- TRUE
 
+# loop until we find the date of interest
 while (x)
 {
-    # explain this thoroughly
-    x <- !grepl("31/1/2007;23:59:00", readLines(conn, n=1))
+    # read one line at a time until we find the last entry prior
+    # to 1/2/2007.  Once this is found, x becomes FALSE and the
+    # loop exits.  We need to stop at the entry before the desired
+    # date so that read.table will start at the correct date
+    x <- !grepl("31/1/2007;23:59:00", readLines(connection, n = 1))
 }
 
-ret <- read.table(conn, header = F, na.strings = "?", sep = ";", nrows = 2880) 
+# read from the connection (2 days X 24 hours X 60 minutes) of data
+# according to the instructions, NAs are represented as ?
+# the data is delimited by ;
+data <- read.table(connection, header = F, na.strings = "?", sep = ";", nrows = 2880) 
 
-close(conn)
+# cleanup
+close(connection)
 
+# Section Two: Massage the data ----
+#
+# read the first line of the file to get the column names for our subset
+colnames(data) <- colnames(read.table(dataFile, header = T, nrows = 1, sep = ";"))
 
-# Section Two ----
-colnames(ret) <- colnames(read.table(dataFile, header = T, nrows = 1, sep = ";"))
+# combine the date and time fields into one time series vector
+results <-paste(data$Date, data$Time, collapse = NULL, sep = ' ')
 
-# Section Three ----
-results <-paste(ret$Date, ret$Time, collapse = NULL, sep = ' ')
-ret$datetime <- strptime(results, format = "%d/%m/%Y  %H:%M:%S")
+# convert the character data to POSIXct data and bind to existing data
+data$datetime <- strptime(results, format = "%d/%m/%Y  %H:%M:%S")
 
+# Section Three: Plot the data ----
+#
+# open the PNG device
 png("plot3.png")
-with(ret, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
-lines(ret$datetime, ret$Sub_metering_2, col = "red")
-lines(ret$datetime, ret$Sub_metering_3, col = "blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = names(ret)[7:9])
 
-# have to add the write statements?
+# plot
+with(data, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+lines(data$datetime, data$Sub_metering_2, col = "red")
+lines(data$datetime, data$Sub_metering_3, col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = names(data)[7:9])
+
+# write
 dev.off()
